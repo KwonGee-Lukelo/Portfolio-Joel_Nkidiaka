8b0410e4d5e907bf0a6aa5e3c062c7fea5e38609
KwonGee-Lukelo <wahabfall17@gmail.com>
2026-02-03 02:10:36 +0100

    feat(script): Configurer swiper

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/*SWIPER**********
+*************************/
+/*const swiper = new swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})*/
+var swiper = new Swiper(".mySwiper", {
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
