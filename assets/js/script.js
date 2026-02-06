d46d09ea7acf5f24012a2332b9d196fd94e795a1
KwonGee-Lukelo <wahabfall17@gmail.com>
2026-02-06 16:51:10 +0100

    feat(header, footer): Ajoute background sur la barre de navigation et l'année sur le footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
+
+const backgroundHeader = () => {
+  const header = document.getElementById("navBar");
+
+  /*Ajout d'une class scroll__style dans la header si on scroll 
+  sur la barre et que l'axe des abscisses devient superieur ou égal à 250px*/
+  this.scrollY >= 150
+    ? header.classList.add("scroll__style")
+    : header.classList.remove("scroll__style");
+};
+window.addEventListener("scroll", backgroundHeader);
+
+let date = document.querySelector('#year');
+let an = new Date().getFullYear();
+console.log(an);
+date.textContent = an;
+console.log(date)
+
 /*SWIPER**********
-*************************/
-/*const swiper = new swiper('.swiper', {
-    direction: 'vertical',
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-})*/
+ *************************/
 var swiper = new Swiper(".mySwiper", {
-      loop: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
